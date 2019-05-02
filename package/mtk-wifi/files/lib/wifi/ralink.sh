450f1b86ae7c9e58dc9d6e3b6313ad3b53ffe081
tolmeros@phoenix <pass>
2019-05-02 04:32:48 +0300

    use uci

--- package/mtk-wifi/files/lib/wifi/ralink.sh
+++ package/mtk-wifi/files/lib/wifi/ralink.sh
@@ -14,34 +14,37 @@ write_ralink() {
 	[ -d /sys/module/$dir ] || return
 	[ -d "/sys/class/net/$dev" ] || return
 
-	cat <<EOF
-config wifi-device	radio0
-	option type     ralink
-	option variant	$devtype
-	option country	CN
-	option hwmode	$mode
-	option htmode	HT20
-	option region 1
-	option channel  6
-	option disabled	0
-	option linkit_mode ap
-config wifi-iface ap
-	option device   radio0
-	option mode	ap
-	option network  lan
-	option ifname   $dev
-	option ssid	Widora-$(cat /sys/class/net/eth0/address|awk -F ":" '{print $5""$6}'| tr a-z A-Z)
-	option encryption none 
-config wifi-iface sta
-	option device   radio0
-	option mode	sta
-	option network  wan
-	option ifname   $sta
-	option led 	mediatek:orange:wifi
-	option ssid	UplinkAp
-	option key	SecretKey
-	option encryption psk
+	MAC=$(dd bs=1 skip=7 count=3 if=/dev/mtd2 2>/dev/null | hexdump -v -n 3 -e '3/1 "%02X"')
+	SSID=MT_Base_${MAC}
+	set wireless.radio${devidx}=wifi-device
+	set wireless.radio${devidx}.type='ralink'
+	set wireless.radio${devidx}.variant='mt7628'
+	set wireless.radio${devidx}.country='CN'
+	set wireless.radio${devidx}.hwmode='11g'
+	set wireless.radio${devidx}.htmode='HT20'
+	set wireless.radio${devidx}.region='1'
+	set wireless.radio${devidx}.channel='6'
+	set wireless.radio${devidx}.disabled='0'
+	set wireless.radio${devidx}.linkit_mode='apsta'
+	set wireless.ap=wifi-iface
+	set wireless.ap.device='radio${devidx}'
+	set wireless.ap.mode='ap'
+	set wireless.ap.network='lan'
+	set wireless.ap.ifname='ra0'
+	set wireless.ap.ssid="$SSID"
+	set wireless.ap.encryption='psk2'
+    set wireless.ap.key="mt20sports"
+	set wireless.sta=wifi-iface
+	set wireless.sta.device='radio${devidx}'
+	set wireless.sta.mode='sta'
+	set wireless.sta.network='wan'
+	set wireless.sta.ifname='apcli0'
+	set wireless.sta.led='mediatek:orange:wifi'
+	set wireless.sta.ssid='UplinkAp'
+	set wireless.sta.key='SecretKey'
+	set wireless.sta.encryption='psk2'
 EOF
+	uci -q commit wireless
 }
 
 detect_ralink() {
